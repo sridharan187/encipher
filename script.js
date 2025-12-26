1d584bddbd13ea57be9bdac92989523414a7871c
sridharan187 <sridharanbalamurugan@gmail.com>
2025-12-26 22:46:09 +0530

    Practice on js
    Practice js data types and operators

---
+++ script.js
@@ -0,0 +1,226 @@
+// var a = 10;
+// var b = 20;
+// var sum = a + b;
+// console.log(sum);
+
+
+// let a1 = 10;
+//  a1 = 20;
+// let a2 =30;
+
+// let add = a1+ a2;
+
+// console.log(add)
+
+
+
+// x= 100;
+// y= 200;
+// x= x + y;
+// console.log(x);
+
+// var str1 = "Hello ";
+// var str1 = "World";
+// let greeting = str1 + str1;
+// console.log(greeting);
+
+
+// let x = "2" + 2;
+// let a = 3;
+// let x = (100 + 50) * a;
+// console.log(x);
+
+
+// const price1 = 5;
+// const price2 = 6;
+// let total = price1 + price2;
+// console.log(total);
+
+
+
+// data types
+
+// str ' ' ""
+
+// no 10
+
+// Boolean true false
+
+// null null
+
+// Undefined  undefined
+
+// Array  []
+
+// object {}
+
+// let name = "John";
+
+// {
+//     console.log(name);
+// }
+// console.log(name);
+
+// let a = [10, 20, 30, 40, 50];
+// console.log(a[2]);
+
+
+// let name = "abc";
+// console.log(name);
+// var a = "" ;
+// let arr = [a,15,true];
+
+// {
+//     arr = [23, "hello", false];
+// }
+// console.log(arr);
+
+
+// str operations
+
+// let firstName = "sri";
+// let lastName = "dharan";
+// console.log(firstName + " " + lastName);
+// console.log(`My name is ${firstName} ${lastName}`);
+
+// let num = 20;
+// console.log(sum = num % 2 == 0);
+// console.log(sum);
+
+// let a = "10";
+// let b = "20";
+// console.log(a + b);
+// console.log(a - b);
+
+
+// console.log(typeof []);
+// console.log(typeof {});
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// let score = null;
+// console.log( score);
+
+// 
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+
+// const a = 10;
+// {
+//   const a = 20;
+    
+// }
+// console.log(a);
+// let id = Symbol("user");
+// let id2 = Symbol("user");
+
+// console.log(id == id2);
+
+// let a = 10;
+//  a += 5;
+// console.log(a);
+
+// let password = "admin123" ;
+// if (password === "admin123") {
+//   console.log("Login Success");
+// }
+
+// Task 
+
+
+let a = "10";
+let b = 5;
+let result1 = (a + b);
+console.log(result1);  //105
+console.log(typeof result1);   //str
+
+let x = 10;
+let y = "10";
+console.log(x == y);  // check only value
+console.log(x === y); //  check value and type
+
+let num = 15;
+let result = num < 0 ? "Negative Number" : num === 0 ? "Zero" : "Positive Number";
+console.log(result);  // ternary operator
+
+ let num = -15;
+
+if (num < 0) {
+    console.log("Negative Number"); 
+} else if (num === 0) {
+    console.log("Zero");
+}
+
+    else {  
+    console.log("Positive Number");
+}
+
+let p = 10;
+let q = 20;
+let r = "30";
+let add = p + q + r;
+console.log(add)  //3030
+console.log(typeof add)  //str
+
+let a = true;
+a = Number(a)  // use N can be capital
+a+=5
+console.log(a)   // 6 
+console.log(typeof a)  //number
+
+let a = 7 ;
+let b = 3 ; 
+let value = a - b; // 4
+console.log(value)
+let great = a < b;   //false  a is greater
+console.log(great)
+
+
+let marks = "85";
+marks = Number(marks)
+if (marks >= 40)
+{
+   console.log("pass")
+}
+else{
+    console.log("fail")   //pass
+}
+
+
+let age = 20;
+let hasID = false;
+
+if (age >=18 && hasID === true ){
+    console.log("Enter ")
+}
+else {
+    console.log("Not Enter")   //not enter 18 false use stick operator 
+}   
+
+let count ;
+console.log(count);                  //undefined
+console.log(typeof count);           //undefined
+count = 10;
+console.log(count);                  //10
+console.log(typeof count);           //number
+
+
+let num1 = "5";
+let num2 = "2";
+
+let addition = num1 + num2;
+console.log(addition);  // 52
+
+let subtraction = num1 - num2;
+console.log(subtraction);  // 3
+let multiplication = num1 * num2;
+console.log(multiplication);
+
+console.log(typeof addition);
+
+console.log(typeof subtraction);
+
+console.log(typeof multiplication);
+
